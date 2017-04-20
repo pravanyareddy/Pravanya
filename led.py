a75fea0bb752dc69775425acc1d892bc05e94fbf
pravanyareddy <pravanya@cykul.com>
2017-04-20 11:22:31 +0000

    changed pin

--- led.py
+++ led.py
@@ -13,10 +13,10 @@ while(x<=1):
 	  GPIO.output(35, 0)
 	  print "OFF 1"
 	  time.sleep(0.5)
-	  GPIO.output(38, 1)
+	  GPIO.output(38, 0)
 	  print "ON 2"
 	  time.sleep(0.5)
-	  GPIO.output(38, 0)
+	  GPIO.output(38, 1)
 	  print "OFF 2"
 	  time.sleep(0.5)
 	  x=x+1
